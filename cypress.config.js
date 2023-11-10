2779402e385ca8b0fb81cf00ffec2f18a7012759
Pati Nogueira <pati.p.nogueira@gmail.com>
2023-11-10 15:13:08 -0300

    atualização

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     reporterOptions: {
       reportDir: 'cypress/results',
       overwrite: false,
-      html: false,
+      html: true,
       json: true,},
     setupNodeEvents(on, config) {
       // implement node event listeners here
